cfc5863c679bd22a984d69d68fb5e485cee7bba7
Sejin Im <sejinim02@gmail.com>
2026-01-11 18:10:05 +0900

    cache, revalidatePath

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -3,13 +3,11 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
 //cache 안에 "home-products"를 key로 하는 캐시가 있는지 확인하고 있으면 그 데이터를 넘겨줌, 없으면 key와 데이터를 저장
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
-    revalidate: 60,
-});
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
 
 async function getInitialProducts() {
     const products = await db.product.findMany({
@@ -35,9 +33,16 @@ export type InitialProducts = Prisma.PromiseReturnType<
 
 export default async function Products() {
     const initialProducts = await getCachedProducts();
+    const revalidate = async () => {
+        "use server";
+        revalidatePath("/home");
+    };
     return (
         <div>
             <ProductList initialProducts={initialProducts} />
+            <form action={revalidate}>
+                <button>Revalidate</button>
+            </form>
             <Link
                 href="/products/add"
                 className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
