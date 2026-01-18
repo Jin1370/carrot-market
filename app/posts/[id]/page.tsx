6ef055419cb5cfeab506b180dd79b6c1c717a371
Sejin Im <sejinim02@gmail.com>
2026-01-18 19:25:34 +0900

    cache post-detail, like-status

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,7 +2,12 @@ import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { formatToTimeAgo } from "@/lib/utils";
 import { EyeIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
-import { revalidatePath } from "next/cache";
+import { HandThumbUpIcon as OutlineHandThumbUpIcon } from "@heroicons/react/24/outline";
+import {
+    revalidatePath,
+    unstable_cache as nextCache,
+    revalidateTag,
+} from "next/cache";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -27,7 +32,6 @@ async function getPost(id: number) {
                 _count: {
                     select: {
                         comments: true,
-                        likes: true,
                     },
                 },
             },
@@ -37,18 +41,40 @@ async function getPost(id: number) {
         return null;
     }
 }
+function getCachedPost(postId: number) {
+    const cachedOperation = nextCache(getPost, ["post-detail"], {
+        tags: [`post-detail-${postId}`],
+        revalidate: 60,
+    });
+    return cachedOperation(postId);
+}
 
-async function getIsLiked(postId: number) {
-    const session = await getSession();
-    const like = await db.like.findUnique({
+async function getLikeStatus(postId: number, userId: number) {
+    //const session = await getSession();
+    const isLiked = await db.like.findUnique({
         where: {
             id: {
                 postId,
-                userId: session.id!,
+                userId,
             },
         },
     });
-    return Boolean(like);
+    const likeCount = await db.like.count({
+        where: {
+            postId,
+        },
+    });
+    return { likeCount, isLiked: Boolean(isLiked) };
+}
+function getCachedLikeStatus(postId: number, userId: number) {
+    const cachedOperation = nextCache(
+        (postId) => getLikeStatus(postId, userId),
+        ["product-like-status"],
+        {
+            tags: [`like-status-${postId}`],
+        },
+    );
+    return cachedOperation(postId);
 }
 
 export default async function PostDetail({
@@ -61,7 +87,7 @@ export default async function PostDetail({
     if (isNaN(id)) {
         return notFound();
     }
-    const post = await getPost(id);
+    const post = await getCachedPost(id);
     if (!post) {
         return notFound();
     }
@@ -75,7 +101,7 @@ export default async function PostDetail({
                     userId: session.id!,
                 },
             });
-            revalidatePath(`/post/${id}`);
+            revalidateTag(`like-status-${id}`);
         } catch (e) {}
     };
     const dislikePost = async () => {
@@ -90,10 +116,11 @@ export default async function PostDetail({
                     },
                 },
             });
-            revalidatePath(`/post/${id}`);
+            revalidateTag(`like-status-${id}`);
         } catch (e) {}
     };
-    const isLiked = await getIsLiked(id);
+    const session = await getSession();
+    const { likeCount, isLiked } = await getCachedLikeStatus(id, session.id!);
     return (
         <div className="p-5 text-white">
             <div className="flex items-center gap-2 mb-2">
@@ -124,10 +151,15 @@ export default async function PostDetail({
                 </div>
                 <form action={isLiked ? dislikePost : likePost}>
                     <button
-                        className={`flex items-center gap-2 text-neutral-400 text-sm border border-neutral-400 rounded-full p-2 hover:bg-neutral-800 transition-colors`}
+                        className={`flex items-center gap-2 text-neutral-400 text-sm border border-neutral-400 rounded-full p-2 transition-colors 
+                            ${isLiked ? "bg-orange-500 text-white border-orange-500" : " hover:bg-neutral-800"}`}
                     >
-                        <HandThumbUpIcon className="size-5" />
-                        <span>공감하기 ({post._count.likes})</span>
+                        {isLiked ? (
+                            <HandThumbUpIcon className="size-5" />
+                        ) : (
+                            <OutlineHandThumbUpIcon className="size-5" />
+                        )}
+                        <span>{likeCount}</span>
                     </button>
                 </form>
             </div>
